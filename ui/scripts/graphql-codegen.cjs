fe8e590e3499b3f2c756f470c4483e066505c10a
Cody Blevins <cody.r.blevins@gmail.com>
2026-02-08 19:11:27 -0500

    ui: make graphql-codegen work with 0 CPUs

---
+++ ui/scripts/graphql-codegen.cjs
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+'use strict';
+
+const os = require('os');
+
+// graphql-codegen CLI uses os.cpus().length to set its internal task concurrency.
+// In some sandboxed environments os.cpus() can be empty, which causes the CLI to
+// throw before generating anything. When that happens, shim a single CPU.
+if (os.cpus().length === 0) {
+  os.cpus = () => [
+    {
+      model: 'sandbox',
+      speed: 0,
+      times: { user: 0, nice: 0, sys: 0, idle: 0, irq: 0 }
+    }
+  ];
+}
+
+// Use a relative path to bypass package "exports" restrictions.
+require('../node_modules/@graphql-codegen/cli/cjs/bin.js');
