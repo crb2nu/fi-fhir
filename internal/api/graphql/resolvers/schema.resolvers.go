0beba3fbd1e2c2f3905bc669373f6bad3e737faa
Cody Blevins <cody.r.blevins@gmail.com>
2026-02-11 18:17:36 -0500

    fix(ci): remove stale gqlgen resolver warning stub

--- internal/api/graphql/resolvers/schema.resolvers.go
+++ internal/api/graphql/resolvers/schema.resolvers.go
@@ -2960,25 +2960,3 @@ func (r *Resolver) Subscription() graphql1.SubscriptionResolver { return &subscr
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
 type subscriptionResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
-/*
-	func (r *workflowResultResolver) RunID(ctx context.Context, obj *model.WorkflowResult) (*string, error) {
-	panic(fmt.Errorf("not implemented: RunID - runId"))
-}
-func (r *workflowResultResolver) Environment(ctx context.Context, obj *model.WorkflowResult) (*string, error) {
-	panic(fmt.Errorf("not implemented: Environment - environment"))
-}
-func (r *workflowResultResolver) VersionID(ctx context.Context, obj *model.WorkflowResult) (*string, error) {
-	panic(fmt.Errorf("not implemented: VersionID - versionId"))
-}
-func (r *Resolver) WorkflowResult() graphql1.WorkflowResultResolver {
-	return &workflowResultResolver{r}
-}
-type workflowResultResolver struct{ *Resolver }
-*/
