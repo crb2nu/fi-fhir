ee5c84bdc0bd8779a7d4206b669ac5a6d5cde94b
loom-spawn <loom-spawn@loom.local>
2026-09-02 16:33:36 +0000

    fix(fhir): advertise build version in production metadata

--- cmd/fi-fhir/main.go
+++ cmd/fi-fhir/main.go
@@ -5216,6 +5216,7 @@ func runServe(args []string) error {
 		TrustedNetworkAuthenticator: securePreviewRuntime.trustedNetwork,
 		HL7IngressPath:              securePreviewRuntime.ingressPath,
 		HL7IngressHandler:           serveMetrics.IngressMiddleware(securePreviewRuntime.ingressHandler),
+		SoftwareVersion:             version,
 	}
 	if !observabilityMode.Legacy() {
 		serverConfig.Health = serveHealth
